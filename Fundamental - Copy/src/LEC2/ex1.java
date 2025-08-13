9a2994e8d193b92a92caaf9f6ede282007e0cc94
honhatminhh <honhatminhocb123@gmail.com>
2025-08-13 20:08:03 +0700

    Add files via upload

---
+++ Fundamental - Copy/src/LEC2/ex1.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package lec2;
+
+/**
+ *
+ * @author Ho Nhat Minh
+ */
+public class ex1 {
+    public static void main(String[] args) {
+        int a = 1+2;
+        double b = 3.5 + 8;
+        double c = 1/2;
+        double d = 4.5 +7;
+        int e = 4 *5 +1;
+        int f = 20%3+2-7;
+        System.out.println("(1): " +a);
+        System.out.println("(2): "+b);
+        System.out.println("(3): "+c);
+        System.out.println("(4): "+d);
+        System.out.println("(5): "+e);
+        System.out.println("(6): "+f);
+    }
+}
---
+++ Fundamental - Copy/src/LEC2/ex22.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package LEC2;
+
+/**
+ *
+ * @author Ho Nhat Minh
+ */
+public class ex22 {
+    public static void main(String[] args) {
+        int x=1;
+        int z=x -x-- - x++ + --x - x +x-- +x;
+        System.out.println("x = "+x);
+        System.out.println("z = "+z);
+    }
+}
