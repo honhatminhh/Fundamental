8a73798017eb4e134421ec8e6a3d9b0410e67596
honhatminhh <honhatminhocb123@gmail.com>
2025-09-25 09:23:40 +0700

    Delete Person_Practice directory

--- Person_Practice/IPerson.java
+++
@@ -1,7 +0,0 @@
-package Person_Practice;
-public interface IPerson {
-    public abstract void addPerson();
-    public abstract boolean updatePerson();
-    public abstract void displayDetails();
-    public abstract double calculateIncome();
-}
